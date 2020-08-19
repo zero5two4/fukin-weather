eff224d990ebeccf52db2108a2bc716f98199e90
ZooH <zero5.two4@gmail.com>
2020-08-20 05:50:45 +0900

    1.2.2 Getting the Location

--- App.js
+++ App.js
@@ -4,13 +4,17 @@ import * as Location from "expo-location";
 import { Alert } from "react-native";
 
 export default class extends React.Component {
+  state = {
+    isLoading: true,
+  };
   getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
       const {
-        coords : {latitude, longitude}
+        coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
       console.log(latitude, longitude);
+      this.setState({ isLoading: false });
     } catch (error) {
       Alert.alert("I Can't find you", "So sad");
     }
@@ -20,6 +24,7 @@ export default class extends React.Component {
     this.getLocation();
   }
   render() {
-    return <Loading />;
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
   }
 }
