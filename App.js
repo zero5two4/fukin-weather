5ee34f8411b6e19689f91a3847baf23f29f73957
ZooH <zero5.two4@gmail.com>
2020-08-21 00:38:07 +0900

    1.2.3 Reinstall nodejs and axios

--- App.js
+++ App.js
@@ -2,10 +2,20 @@ import React from "react";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 import { Alert } from "react-native";
+import axios from "axios";
+
+const API_KEY = "4492d96517b54b3f5a6df32a83e694e5";
+// api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}
 
 export default class extends React.Component {
   state = {
     isLoading: true,
+  };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
   };
   getLocation = async () => {
     try {
@@ -14,6 +24,7 @@ export default class extends React.Component {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
       console.log(latitude, longitude);
+      this.getWeather(latitude, longitude);
       this.setState({ isLoading: false });
     } catch (error) {
       Alert.alert("I Can't find you", "So sad");
